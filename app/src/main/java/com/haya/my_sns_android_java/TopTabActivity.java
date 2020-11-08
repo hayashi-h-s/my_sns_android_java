78feaeaeaff80b5bea2a82628b03a1ebc15d1a79
shinji hayamizu <max9173627@gmail.com>
2020-11-08 15:08:33 +0900

    aadd

---
+++ app/src/main/java/com/haya/my_sns_android_java/TopTabActivity.java
@@ -0,0 +1,18 @@
+package com.haya.my_sns_android_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TopTabActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_tab);
+
+
+
+
+  }
+}
